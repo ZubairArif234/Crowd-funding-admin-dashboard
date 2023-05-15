56fae52e28efc780a6aecccfd6960253126711a1
zubair <64973610+FaizGhouri@users.noreply.github.com>
2023-05-15 20:02:31 +0500

    mui added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './pages/dashboard';
+import PersistentDrawerLeft from './pages/dashboard2';
 
 function App() {
   return (
     <div>
-    <Dashboard/>
+    {/* <Dashboard/> */}
+    <PersistentDrawerLeft/>
     </div>
   );
 }
--- src/components/images/dashboardtopsmallcards.jsx
+++ src/components/images/dashboardtopsmallcards.jsx
@@ -1,13 +1,20 @@
 
 
 import '../../dashboard.css'
-const Smallcard = ({bordercolour}) =>{
+const Smallcard = ({bordercolour , graphimg}) =>{
     return(
         <div className='dashboardcardtopdiv'>
 {/* <p>hi</p> */}
-<div className='dashboardcardtopone' style={{borderTop:bordercolour}}>
+<div className='dashboardcardtopone' style={{borderTop:bordercolour , display:'flex' , justifyContent:'space-between', flexWrap:'wrap'}}>
+    <div>
+
     <p className='dashboardcardtoponesmalltext'>Fund Raised</p>
     <p className='dashboardcardtoponebigtext'>$1,512</p>
+    </div>
+    <div className='graphimgtopboxes' >
+
+    <img src={graphimg} style={{width:'100%'}}/>
+    </div>
 </div>
         </div>
     )
